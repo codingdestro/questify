f31b4a68e96e3c8a93139b79c1985f2c22fd748a
codingdestro <codingdestro@gmail.com>
2025-10-28 14:37:20 +0530

    add: dasboard page with dummy data

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,37 @@
+import Dashboard from "@/components/dashboard";
+
+const dummyDashboard = {
+  username: "Anas",
+  currentMarks: 50,
+  totalMarks: 100,
+  streak: 2,
+  levels: [
+    {
+        name: "Beginner",
+        currentMarks: 20,
+        totalMarks: 30,
+        isCompleted: true,
+    }
+  ],
+  revisionHistory: [
+    {
+        id: "1",
+        question: "What is React?",
+        isCorrect: true,
+        timestamp: new Date(),
+   }
+  ],
+};
+
+export default function page() {
+  return (
+    <Dashboard
+      currentMarks={dummyDashboard.currentMarks}
+      totalMarks={dummyDashboard.totalMarks}
+      username={dummyDashboard.username}
+      streak={dummyDashboard.streak}
+      levels={dummyDashboard.levels}
+      revisionHistory={dummyDashboard.revisionHistory}
+    />
+  );
+}
